9ff4f22294e5ce2303ca976f33b9c647abba441a
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2024-04-21 21:25:33 +0200

    Added support for preview and sancho networks (#1)

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,26 +2,38 @@ import axios from "axios";
 import config from "config";
 import { FastifyInstance } from "fastify";
 
+type Network = "mainnet" | "preprod" | "preview" | "sanchonet";
+
 const proxyRoute = async (server: FastifyInstance) => {
-  server.get<{ Params: { network: "mainnet" | "preprod"; postfix: string } }>(
+  server.get<{ Params: { network: Network; postfix: string } }>(
     "/api/blockfrost/:network/*",
     async (request, reply) => {
       const { network } = request.params;
+      const projectId = config.get<string>("blockfrost.project_id");
+      const blockfrostNetwork = network.toLowerCase();
+      const configName = `blockfrost.${blockfrostNetwork}.url`;
 
-      const baseUrl =
-        network.toLowerCase() === "mainnet"
-          ? config.get<string>("blockfrost.mainnet.url")
-          : config.get<string>("blockfrost.preprod.url");
-      const headers = {
-        headers: {
-          project_id: config.get<string>("blockfrost.project_id"),
-        },
-      };
+      if (!projectId.startsWith(blockfrostNetwork.slice(0, 7))) {
+        reply.code(400).send({ error: "Invalid network" });
+        return;
+      }
+
+      if (!config.has(configName)) {
+        reply.code(500).send({ error: "Network is misconfigured" });
+        return;
+      }
+
+      const baseUrl = config.get<string>(configName);
 
       try {
-        const url = `${baseUrl}${request.url.substring(`/api/blockfrost/${network}`.length)}`;
+        const path = sanitize(request.url);
+        const url = `${baseUrl}${path.substring(`/api/blockfrost/${network}`.length)}`;
         console.debug("URL:", url);
-        const response = await axios.get(url, headers);
+        const response = await axios.get(url, {
+          headers: {
+            project_id: projectId,
+          },
+        });
         reply.code(response.status).send(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -30,5 +42,7 @@ const proxyRoute = async (server: FastifyInstance) => {
     },
   );
 };
+
+const sanitize = (path: string) => path.replace(/[^a-zA-Z0-9/_.-]/g, "");
 
 export default proxyRoute;
