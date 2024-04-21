00bc6b7eeb57a0d650eeb7358e3e7a3f1e9b8611
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2024-04-21 21:07:49 +0200

    Added more sanity checks

--- src/proxy.ts
+++ src/proxy.ts
@@ -9,19 +9,30 @@ const proxyRoute = async (server: FastifyInstance) => {
     "/api/blockfrost/:network/*",
     async (request, reply) => {
       const { network } = request.params;
-      const baseUrl = config.get<string>(
-        `blockfrost.${network.toLowerCase()}.url`,
-      );
-      const headers = {
-        headers: {
-          project_id: config.get<string>("blockfrost.project_id"),
-        },
-      };
+      const projectId = config.get<string>("blockfrost.project_id");
+      const blockfrostNetwork = network.toLowerCase();
+      const configName = `blockfrost.${blockfrostNetwork}.url`;
+
+      if (!projectId.startsWith(blockfrostNetwork.slice(0, 7))) {
+        reply.code(400).send({ error: "Invalid network" });
+        return;
+      }
+
+      if (!config.has(configName)) {
+        reply.code(500).send({ error: "Network is misconfigured" });
+        return;
+      }
+
+      const baseUrl = config.get<string>(configName);
 
       try {
         const url = `${baseUrl}${request.url.substring(`/api/blockfrost/${network}`.length)}`;
         console.debug("URL:", url);
-        const response = await axios.get(url, headers);
+        const response = await axios.get(url, {
+          headers: {
+            project_id: projectId,
+          },
+        });
         reply.code(response.status).send(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
