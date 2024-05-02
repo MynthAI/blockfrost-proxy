ec8a2eaf5e4c1bf039e44fc5cf0819b0c054d466
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2024-05-02 18:19:49 +0200

    This doesn't need to be awaited

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const start = async () => {
   const port = config.get<number>("server.port");
 
   const server = Fastify({
-    logger: await setupLogging(),
+    logger: setupLogging(),
     disableRequestLogging: true,
   });
 
